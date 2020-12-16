9a30b16152eebbbb54ba06a11c6b04640f108c03
poonkit <ankitraj25@hotmail.com>
2020-12-16 16:26:07 +0530

    Added website url for contact form submission

--- html5-contact-form-send-email/handler.php
+++ html5-contact-form-send-email/handler.php
@@ -11,7 +11,7 @@ require_once './vendor/autoload.php';
 use FormGuide\Handlx\FormHandler;
 
 
-$pp = new FormHandler();
+$pp = new FormHandler(); 
 
 $validator = $pp->getValidator();
 $validator->fields(['name','email'])->areRequired()->maxLength(50);
@@ -19,9 +19,8 @@ $validator->field('email')->isEmail();
 $validator->field('comments')->maxLength(6000);
 
 
-$mailer = $pp->getMailer();
-$mailer->setFrom('www.techglu.com','Form',false);
 
-$fh->sendEmailTo(['poojaa.todkar0594@gmail.com', 'poojavk21@gmail.com']);
+
+$fh->sendEmailTo(['poojavk21@gmail.com', 'aasaysrvt@gmail.com']);
 
 echo $pp->process($_POST);
